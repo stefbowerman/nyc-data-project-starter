1c17d1b85efce27a02942867e4193c80b250054a
Stefan Bowerman <stefbowerman@gmail.com>
2023-03-30 14:36:11 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# NYC Data - Project Starter
+
+### Resources
+- [NYC Open Data](https://opendata.cityofnewyork.us/)
+- [Browse Data Sets](https://data.cityofnewyork.us/browse?sortBy=most_accessed&utf8=%E2%9C%93)
+- [Developer Settings](https://data.cityofnewyork.us/profile/edit/developer_settings) (You must be logged in!)
---
+++ script.js
@@ -0,0 +1,14 @@
+const APP_TOKEN = 'WdJzO2HhdsAyoAlq2V27X1V3Y' // <- Replace this with your app token
+const RESOURCE_ID = 'jb7j-dtam' // <- Replace this with the ID for the data resource that you want to look up
+const LIMIT = 20 // <- Replace this with the number of records you want to pull
+
+// Construct the URL that we need to make requests
+const url = `https://data.cityofnewyork.us/resource/${RESOURCE_ID}.json?$limit=${LIMIT}&$$app_token=${APP_TOKEN}`
+
+console.log(`Fetching url - ${url}`)
+
+fetch(url)
+  .then((response) => response.json())
+  .then((json) => {
+      console.log(json)
+  });
