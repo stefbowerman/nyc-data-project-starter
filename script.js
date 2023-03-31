9b52dcc9eb41f98908faa8c89d5bf6c5493e9be5
Stefan Bowerman <stefbowerman@gmail.com>
2023-03-31 11:09:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const APP_TOKEN = 'WdJzO2HhdsAyoAlq2V27X1V3Y' // <- Replace this with your app token
-const RESOURCE_ID = 'jb7j-dtam' // <- Replace this with the ID for the data resource that you want to look up
+const APP_TOKEN = 'rvPvn1wkUHNFpw28tRW4tsCtv' // <- Replace this with your app token
+const DATASET_IDENTIFIER = 'h9gi-nx95' // <- Replace this with the ID for the data resource that you want to look up
 const LIMIT = 20 // <- Replace this with the number of records you want to pull
 
 // Construct the URL that we need to make requests
-const url = `https://data.cityofnewyork.us/resource/${RESOURCE_ID}.json?$limit=${LIMIT}&$$app_token=${APP_TOKEN}`
+const url = `https://data.cityofnewyork.us/resource/${DATASET_IDENTIFIER}.json?$limit=${LIMIT}&$$app_token=${APP_TOKEN}`
 
 console.log(`Fetching url - ${url}`)
 
